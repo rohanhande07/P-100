7e325f764014b4c1d6838fecccaaa2a4d249bf01
rohanhande07 <64739098+rohanhande07@users.noreply.github.com>
2022-01-08 11:43:11 +0000

    Add files via upload

--- Atm.py
+++ Atm.py
@@ -11,3 +11,25 @@ class Atm(object):
 
 Lloyds = Atm("1346", "876")
 Lloyds.cardNumber
+
+def main():
+    Card_number = input("insert your card number:- ")
+    pin_number  = input("enter your pin number:- ")
+
+    new_user =  Atm(Card_number ,pin_number)
+
+    print("Choose your activity ")
+    print("1.Balance Enquriy   2.withdrawl")
+    activity = int(input("enter activity number :- "))
+
+    if (activity == 1):
+        new_user.check_balance()
+    elif (activity == 2):
+        amount = int(input("enter the amount:- "))
+        new_user.withdrawl(amount)
+    else:
+        print("enter a valid number")
+
+
+if __name__ == "__main__":
+    main()
